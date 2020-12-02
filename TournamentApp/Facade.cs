d8e0022ee293f77366f3729c116ee266c9954ae0
Mateusz <mateusz@MacBook-Air-Mateusz.local>
2020-12-02 23:52:48 +0100

    Edit

--- TournamentApp/Facade.cs
+++ TournamentApp/Facade.cs
@@ -7,41 +7,38 @@ namespace TournamentApp
     class Facade
     {
         Operations operations = new Operations();
+        SaveToFile saveToFile = new SaveToFile();
         List<Fighter> fighters = new List<Fighter>();
 
         public void Menu()
         {
-            Console.WriteLine("Strona główna aplikacji turniejowej gali FightClub, wybierz opcje: \n");
+            Console.WriteLine("Strona główna aplikacji turniejowej gali TwojStaryZawadiaka, wybierz opcje: \n");
             Console.WriteLine("1 - Rejestracja zawodnika | 2 - Lista Zawodników | 3 - Informacje | 4 - Wyjście");
             int option;
+
+
+
+            option = int.Parse(Console.ReadLine());
+            switch (option)
             {
-
-
-                option = int.Parse(Console.ReadLine());
-                switch (option)
-                {
-                    case 1:
-                        operations.Register(fighters);
-                        Menu();
-                        break;
-                    case 2:
-                        operations.FightersList(fighters);
-                        Menu();
-                        break;
-                    case 3:
-                        Console.WriteLine("Wykorzystane wzorce: Budowniczy, Fasada");
-                        Menu();
-                        break;
-                    case 4:
-                        Console.WriteLine("Wciśnij dowolny klawisz...");
-                        Console.ReadKey();
-                        return;
-
-
-                }
+                case 1:
+                    operations.Register(fighters);
+                    saveToFile.Save(fighters);
+                    Menu();
+                    break;
+                case 2:
+                    operations.FightersList(fighters);
+                    Menu();
+                    break;
+                case 4:
+                    Console.WriteLine("Wciśnij dowolny klawisz...");
+                    Console.ReadKey();
+                    return;
 
             }
+
         }
     }
 }
+    
 
--- TournamentApp/SaveToFile.cs
+++ TournamentApp/SaveToFile.cs
@@ -1,10 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
 namespace TournamentApp
 {
-    public class SaveToFile
+    class SaveToFile
     {
-        public SaveToFile()
+        Operations _operations = new Operations();
+        private string filePath = "zawodnicy.txt";
+        public void Save(List<Fighter> fighters)
         {
+            File.Delete(filePath);
+            foreach (var fighter in fighters)
+            {
+                var fighterString = fighter.name + " | " + fighter.surname + " | " + "Waga: " + fighter.weight + " kg. | Styl walki: " + _operations.ConvertStyle(fighter.style) + " | " + "Liczba stoczonych walk: " + fighter.fightsCount+"\n";
+                File.AppendAllText(filePath, fighterString);
+
+            }
         }
+
+
     }
 }
